930b593b91ae240c79e6776032fda2c12f6d1449
Razvan <razvansau9@gmail.com>
2024-05-05 15:32:49 +0300

    Engine preparation: Added the configuration handler

---
+++ src/engine/configuration/configuration.py
@@ -0,0 +1,29 @@
+import yaml, os
+
+defaultConfig = """# Snaptivate configuration.yaml file
+
+# Provide the API key that you'll get when you access the following link:
+# https://api.imgbb.com/
+# Make sure that you are logged in first! Example:
+# IMGBB_API_KEY: \"abcdefghjkl\"
+IMGBB_API_KEY: \"\"
+"""
+
+def getConfigurationValue(value: str) -> object:
+    # Creates the directory if it's not already
+    directory = os.path.join(os.path.expanduser('~'), 'Documents', 'Snaptivate')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Check if the file exists, if not, creates it with the default configuration
+    file_path = os.path.join(directory, 'configuration.yaml')
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as file:
+            file.write(defaultConfig)
+    
+    # Reads the configuration.yaml file and returns the value requested.
+    file_path = os.path.join(directory, 'configuration.yaml')
+    with open(file_path, 'r') as file:
+        data = yaml.safe_load(file)
+        return data[value]
+    
--- src/engine/processes/upload.py
+++ src/engine/processes/upload.py
@@ -1,8 +1,27 @@
+import requests, os, sys, time
+
 from processes import search
+from configuration import configuration
 
 def start() -> None:
-    # Needs logic to save the file to a temporary server and redirect the URL to the search.start() function
-    url = ""
+    file = os.path.join(os.path.expanduser('~'), 'Documents', 'Snaptivate', 'latest.jpg')
 
-    # Jumping to the searching process
-    search.start(url)
+    upload_url = "https://api.imgbb.com/1/upload"
+    params = {
+        "key": configuration.getConfigurationValue("IMGBB_API_KEY"),
+        "expiration": 300
+    }
+    files = {"image": open(file, "rb")}
+
+    response = requests.post(upload_url, params = params, files = files)
+    json_response = response.json()
+
+    if response.status_code == 200 and json_response["success"]:
+        search.start(json_response["data"]["url"])
+    else:
+        print("Error: Couldn't access the ImgBB API; make sure you've provided the correct API key.")
+        print("Provide your API key in the configuration.yaml file, located in:", f'{os.path.expanduser('~').replace('\\', '/')}/Documents/Snaptivate')
+        print("The application is closing in 5 seconds...")
+
+        time.sleep(5)
+        sys.exit()
